c6a3c577de7c0849f76b8c297f3abdc4f8ac26e7
Raphael Simonetti <raphasimonettti@gmail,com>
2023-07-02 16:14:18 -0300

    Algoritmo Finalizado

--- Pesquisa_Binaria/src/searchBinary/Principal.java
+++ Pesquisa_Binaria/src/searchBinary/Principal.java
@@ -1,14 +1,34 @@
 package searchBinary;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         int[] lista = {1,2,3,4,5,6,7,8,9,10};
-        int item = 10;
+        int item ;
         int inicio = 0;
         int fim = lista.length - 1;
+        Scanner scanner = new Scanner(System.in) ;
 
-        int resultado = FunctionSearch.Binary_Search(lista, item, inicio, fim);
+        while (true) {
+            System.out.println("Digite o N que deseja saber a posição em um Array de 10 números");
 
-        System.out.println("A Número do"+" "+item+" "+"No vetor é a posição:"+" "+resultado);
+            if (scanner.hasNextInt()) {
+                item = scanner.nextInt();
+                int resultado = FunctionSearch.Binary_Search(lista, item, inicio, fim);
+                scanner.close();
+
+                if(resultado == -1) {
+                    System.out.println("O número não existe no vetor.");
+                } else {
+                    System.out.println("A posição do número "+ item +" no vetor é: "+ resultado);
+                }
+                
+                break; 
+            } else {
+                System.out.println("Por favor, digite um número inteiro.");
+                scanner.nextLine(); 
+            }
+        }
     }
 }
